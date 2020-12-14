efe58448f9e0cfedc7d600ad0e4074ae62d36d33
canyon15 <rog14009@byui.edu>
2020-12-14 14:44:39 -0700

    first push

---
+++ MainActivity.java
@@ -0,0 +1,59 @@
+package com.example.tryagnmaps;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.esri.arcgisruntime.mapping.ArcGISMap;
+import com.esri.arcgisruntime.mapping.Basemap;
+import com.esri.arcgisruntime.mapping.view.MapView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private MapView mMapView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mMapView = findViewById(R.id.mapAppView);
+        setupMap();
+    }
+
+
+    @Override
+    protected void onPause() {
+        if (mMapView != null) {
+            mMapView.pause();
+        }
+        super.onPause();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (mMapView != null) {
+            mMapView.resume();
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        if (mMapView != null) {
+            mMapView.dispose();
+        }
+        super.onDestroy();
+    }
+
+    private void setupMap() {
+        if (mMapView != null) {
+            Basemap.Type basemapType = Basemap.Type.STREETS_VECTOR;
+            double latitude = 43.8231;
+            double longitude = -111.7924;
+            int levelOfDetail = 9;
+            ArcGISMap map = new ArcGISMap(basemapType, latitude, longitude, levelOfDetail);
+            mMapView.setMap(map);
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,7 @@
+#tryagnmaps
+
+java and xml files for map application
+
+##Functions setUpMap: initializes map to view rexburg when opened.
+
+##Dev Enviroment Created in Android Studio, using Java, XML, and arcGIS.
